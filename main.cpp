87ed8f9a4a11b70f8b733a67dd9db55547201b68
AndrewMagdy <andrewtawf@gmail.com>
2015-12-13 14:59:17 +0200

    Yes

--- main.cpp
+++ main.cpp
@@ -26,28 +26,35 @@
 #ifdef __APPLE__
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
+#include <GLUT/glut.h>
 #include <GLFW/glfw3.h>
 #elif __linux__
 #include <GL/gl.h>
 #include <GL/glu.h>
+#include <GLUT/glut.h>
 #include <GLFW/glfw3.h>
 #else
 #error "Unknown Compiler"
 #endif
 
 
-#include <iostream>
-#include <vector>
-#include <string>
-
 #include "mesh.h"
 #include "texture.h"
 
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 
 
 int error = 0;
+int mouseX, mouseY;
+bool mouseleftdown = false;
 float rot = 0.0f;
 float rotb = 40.0f;
+
+int GposX, GposZ;
+bool selected = false;
 
 bool anim_cata = true;
 // Meshes
@@ -57,8 +64,11 @@ Mesh* catapult_1;
 // Textures
 GLuint texWood;
 GLuint texStone;
+GLuint texGrass;
 
 
+GLdouble zNear = 0.1;
+GLdouble zFar = 1000000;
 
 const struct Camera {
 	float	eyeX ;
@@ -68,8 +78,17 @@ const struct Camera {
 	float	centerY;
 	float	centerZ;
 	float	fov;
-} defaultCamera = {20, 10, 20, 20, 0, 0, 56};
+} defaultCamera = {20, 470, 70, 20, 0, 0, 70};
 Camera camera = defaultCamera;
+
+const struct Grid {
+	int startX;
+	int startY;
+	int stepX;
+	int stepY;
+	int endX;
+	int endY;
+} grid = {-200, -200, 43, 43, 200, 200 };
 
 
 int getLatestError() {
@@ -79,7 +98,7 @@ int getLatestError() {
 
 void initGL() {
 
-	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glm::vec3 lightposition = glm::vec3(0, 500, 0);
@@ -102,6 +121,7 @@ void initGL() {
 
 	texStone = Texture::loadPngTexture("Textures/stone.jpg");
 
+	texGrass = Texture::loadPngTexture("Textures/grass.png");
 }
 void animateCatapult () {
 	if (rotb <= 0) {
@@ -112,7 +132,7 @@ void animateCatapult () {
 		glTranslatef(20, 0, 0);
 		glRotatef(45, 0, 1, 0);
 		glRotatef(rotb, 1, 0, 0);
-		glScalef(2, 2, 2);
+		//glScalef(2, 2, 2);
 		glBindTexture(GL_TEXTURE_2D, texWood);
 		catapult_1->render();
 		glBindTexture(GL_TEXTURE_2D, 0);
@@ -124,7 +144,7 @@ void animateCatapult () {
 		glTranslatef(20, 0, 0);
 		glRotatef(45, 0, 1, 0);
 		glRotatef(rot, 1, 0, 0);
-		glScalef(2, 2, 2);
+		//glScalef(2, 2, 2);
 		glBindTexture(GL_TEXTURE_2D, texWood);
 		catapult_1->render();
 		glBindTexture(GL_TEXTURE_2D, 0);
@@ -136,7 +156,7 @@ void drawCatapult () {
 	glPushMatrix();
 	glTranslatef(20, 0, 0);
 	glRotatef(45, 0, 1, 0);
-	glScalef(2, 2, 2);
+	//glScalef(2, 2, 2);
 	glBindTexture(GL_TEXTURE_2D, texWood);
 	catapult_0->render();
 	glBindTexture(GL_TEXTURE_2D, 0);
@@ -149,7 +169,7 @@ void drawCatapult () {
 		glPushMatrix();
 		glTranslatef(20, 0, 0);
 		glRotatef(45, 0, 1, 0);
-		glScalef(2, 2, 2);
+		//glScalef(2, 2, 2);
 		glBindTexture(GL_TEXTURE_2D, texWood);
 		catapult_1->render();
 		glBindTexture(GL_TEXTURE_2D, 0);
@@ -157,6 +177,98 @@ void drawCatapult () {
 	}
 }
 
+void drawSmallGrid(float x, float y, int gridX , int gridZ){
+	for (int i = 0; i < 3; ++i) {
+		for(int j = 0; j < 3; ++j){
+			glEnable(GL_TEXTURE_2D);	// Enable 2D texturing
+			if(selected && (gridX * 3) + j == GposX && (gridZ * 3) + i == GposZ ) {
+				glBindTexture(GL_TEXTURE_2D, texStone);
+			}
+			else {
+				glBindTexture(GL_TEXTURE_2D, texGrass);
+			}
+			glTexCoord2f(0, 0);		// Set tex coordinates ( Using (0,0) -> (5,5) with texture wrapping set to GL_REPEAT to simulate the ground repeated grass texture).
+			glColor3f(0,1,0);
+			glPushMatrix();
+			glBegin(GL_QUADS);
+			glNormal3f(0, 1, 0);	// Set quad normal direction.
+			glTexCoord2f(0, 0);		// Set tex coordinates ( Using (0,0) -> (5,5) with texture wrapping set to GL_REPEAT to simulate the ground repeated grass texture).
+			glVertex3f(x + (43 * i), 1, y + (43 * j));
+			glTexCoord2f(5, 0);
+			glVertex3f(x + 40 + (43 * i) , 1, y + (43 * j));
+			glTexCoord2f(5, 5);
+			glVertex3f(x + 40 + (43 * i), 1, y + 40 + (43 * j));
+			glTexCoord2f(0, 5);
+			glVertex3f(x + (43 * i), 1, y + 40 + (43 * j));
+
+
+			glEnd();
+			glPopMatrix();
+		}
+	}
+
+}
+void drawGrid () {
+	for (int i = 0; i < 3; ++i) {
+		for(int j = 0; j < 3; ++j){
+			drawSmallGrid(-200 + (133 * i), -200 + (133 * j), j, i);
+		}
+	}
+
+}
+std::vector<GLdouble> GetOGLPos(double x, double y)
+{
+	GLint viewport[4];
+	GLdouble modelview[16];
+	GLdouble projection[16];
+	GLfloat winX, winY, winZ;
+	GLdouble posX, posY, posZ;
+
+	glGetDoublev( GL_MODELVIEW_MATRIX, modelview );
+	glGetDoublev( GL_PROJECTION_MATRIX, projection );
+	glGetIntegerv( GL_VIEWPORT, viewport );
+
+	winX = (float)x;
+	winY = (float)viewport[3] - (float)y;
+	glReadPixels( x, int(winY), 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &winZ );
+
+	gluUnProject( winX, winY, winZ, modelview, projection, viewport, &posX, &posY, &posZ);
+
+	GLdouble arr[] = {posX,posY,posZ};
+	vector<GLdouble> res (arr, arr + sizeof(arr) / sizeof(arr[0]) );
+	return res;
+}
+
+
+
+void RenderGround()
+{
+	glDisable(GL_LIGHTING);	// Disable lighting
+
+	glColor3f(0.6, 0.6, 0.6);	// Dim the ground texture a bit
+
+	glEnable(GL_TEXTURE_2D);	// Enable 2D texturing
+
+	glBindTexture(GL_TEXTURE_2D, texGrass);	// Bind the ground texture
+
+	glPushMatrix();
+	glBegin(GL_QUADS);
+	glNormal3f(0, 1, 0);	// Set quad normal direction.
+	glTexCoord2f(0, 0);		// Set tex coordinates ( Using (0,0) -> (5,5) with texture wrapping set to GL_REPEAT to simulate the ground repeated grass texture).
+	glVertex3f(-200, 0, -200);
+	glTexCoord2f(5, 0);
+	glVertex3f(200, 0, -200);
+	glTexCoord2f(5, 5);
+	glVertex3f(200, 0, 200);
+	glTexCoord2f(0, 5);
+	glVertex3f(-200, 0, 200);
+	glEnd();
+	glPopMatrix();
+
+	glEnable(GL_LIGHTING);	// Enable lighting again for other entites coming throung the pipeline.
+
+	glColor3f(1, 1, 1);	// Set material back to white instead of grey used for the ground texture.
+}
 void render() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -168,12 +280,13 @@ void render() {
 	glPopMatrix();
 
 	drawCatapult();
-
+	RenderGround();
+	drawGrid();
 
 	//The Camera
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(camera.fov, 300 / 300, 0.1f, 300.0f);
+	gluPerspective(camera.fov, 300 / 300, zNear, zFar);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
@@ -222,31 +335,24 @@ void key_callback(GLFWwindow* window, int key, int scanccode, int action, int mo
 	}
 }
 
-void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
-{
-	//can also get this with glfwGetCursorPos(window, &xpos, &ypos);
-	//std::cout << "[" << xpos << ", " << ypos << "]\n";
-}
-
-void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
-{
-	if (action == GLFW_PRESS)
-	{
-		switch (button)
-		{
-			case GLFW_MOUSE_BUTTON_RIGHT:
-			break;
-
-			case GLFW_MOUSE_BUTTON_MIDDLE:
-			break;
-
-			case GLFW_MOUSE_BUTTON_LEFT:
-			break;
-
-			default:
-			break;
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+		vector<GLdouble> oglPos = GetOGLPos(xpos, ypos);
+		if(oglPos[2] >= -200 && oglPos[2] <= 200 ) {
+			if(oglPos[0] >=-200 && oglPos[0] <= 200) {
+				 GposX = ((oglPos[2] - grid.startX)/grid.stepX);
+				 GposZ = ((oglPos[0] - grid.startY)/grid.stepY);
+				 selected = true;
+				 std::cout << GposX << " " << GposZ << std::endl;
+				//rowSelection = true;
+				//row = 4-GposZ ;
+				//col = GposX ;
+			}
 		}
 	}
+
 }
 
 int main(int argc, char* argv[])
@@ -262,7 +368,6 @@ int main(int argc, char* argv[])
 		return getLatestError();
 	}
 
-	glfwSetCursorPosCallback(window, cursor_position_callback);
 	glfwSetMouseButtonCallback(window, mouse_button_callback);
 	glfwSetKeyCallback(window, key_callback);
 	glfwMakeContextCurrent(window);
